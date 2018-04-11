1cce0fda77ea690c517c16d340e7ce263747051f
Pruthvi Sasank Mandava (Trianz) <Pruthvi.Mandava@trianz.com>
2018-04-11 17:25:43 +0530

    removed navbar

--- app.module.ts
+++ app.module.ts
@@ -2,19 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { NgForm } from '@angular/forms';
-
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
-import { NavbarComponent } from './navbar/navbar.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
-    
     SidebarComponent,
-    NavbarComponent,
-     
   ],
   imports: [
     BrowserModule, FormsModule,
